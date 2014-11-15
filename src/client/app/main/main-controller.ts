779279c3a1b09e9a3a6f6415e6a94d123cfa7c33
Ole Rehmsen <ole@rehmsen.de>
2014-11-15 22:16:45 +0100

    Extract State class.
    Not a service because services are singleton, but we will likely want to track two states: The state the red line is indicating, and the final state.
    There are a couple more fields on MainController that I would like to move to State, will do so in separate commits.

--- src/client/app/main/main-controller.ts
+++ src/client/app/main/main-controller.ts
@@ -5,10 +5,10 @@ import core = require('../../components/aoe2/model/core');
 import assignments = require('../../components/aoe2/model/assignments');
 import BuildOrderService = require('../../components/aoe2/model/build-order-service');
 import RulesService = require('../../components/aoe2/model/rules-service');
-
+import State = require('../../components/aoe2/model/state');
 
 class MainController {
-  timeScale: number;
+  timeScale: number = 3;
 
   age: core.IAge;
 
@@ -17,21 +17,12 @@ class MainController {
   settings: core.ISettings;
   worker: build.Unit;
   assignmentFactory: assignments.AssignmentFactory;
-  currentState: core.IState;
-
-  private _time: number;
+  currentState: State;
 
 
   constructor($scope, 
-      public rulesService: RulesService, 
-      public buildOrderService: BuildOrderService) {
-    this.rulesService.load('assets/rules/aoc.yaml').then(function() {
-      this.age = this.rulesService.ages[0];
-      this.assignmentFactory = new assignments.AssignmentFactory(
-          this.rulesService.resourceSources);
-      this.time = 0;
-    }.bind(this));
-
+      public buildOrderService: BuildOrderService,
+      public rulesService: RulesService) {
     this.hasBuilding = {
       'town_center': true
     };
@@ -40,52 +31,21 @@ class MainController {
       resources: 'low',
       allTechs: true
     };
+    this.currentState = new State(
+        buildOrderService, rulesService, this.settings);
 
-    this.timeScale = 3;
-  }
-
-  set time(time: number) {
-    this._time = time;
-    this.currentState = this.interpolateState(time);
-  }
-
-  get time(): number {
-    return this._time;
-  }
-
-  interpolateState(time: number) {
-    if (!this.rulesService.loaded) {
-      return;
-    }
-    time = time !== undefined ? time : Number.MAX_VALUE;
-    var state = new core.State(
-      angular.copy(this.rulesService.startResources[this.settings.resources]),
-      4, 5, {'idle': new assignments.IdleAssignment(3)});
-    var lastTime = 0;
-    this.buildOrderService.buildOrder.forEach(function(item) {
-      var delta = Math.min(item.start, time) - lastTime;
-      angular.forEach(state.assignments, function(assignment: core.IAssignment) {
-        assignment.apply(delta, state);
-      }, this);
-
-      lastTime += delta;
-      if (item.start > time) {
-        return;
-      }
-      item.apply(state);
-    });
-    var delta = time - lastTime;
-    angular.forEach(state.assignments, function(assignment: core.IAssignment) {
-      assignment.apply(delta, state);
-    }, this);
-
-    return state;
+    this.rulesService.load('assets/rules/aoc.yaml').then(function() {
+      this.age = this.rulesService.ages[0];
+      this.assignmentFactory = new assignments.AssignmentFactory(
+          this.rulesService.resourceSources);
+      this.currentState.time = 0;
+    }.bind(this));
   }
 
   build(building: build.Building): void {
     building.source = 'villager';
     var queue = this.buildOrderService.enqueueBuildableItem(building);
-    this.time = queue.start + queue.length;
+    this.currentState.time = queue.start + queue.length;
     var completionTime = queue.length;
     this.buildOrderService.queues.push({
       source: building.id,
@@ -99,7 +59,7 @@ class MainController {
   research(tech: build.Technology): void {
     this.hasTechnology[tech.id] = true;
     var queue = this.buildOrderService.enqueueBuildableItem(tech);
-    this.time = queue.start + queue.length;
+    this.currentState.time = queue.start + queue.length;
   }
 
   train(unit: build.Unit): void {
@@ -107,13 +67,13 @@ class MainController {
       this.worker = unit;
     } else {
       var queue = this.buildOrderService.enqueueBuildableItem(unit);
-      this.time = queue.start + queue.length;
+      this.currentState.time = queue.start + queue.length;
     }
   }
 
   assign(task: string): void {
     var queue = this.buildOrderService.enqueueBuildableItem(this.worker);
-    this.time = queue.start + queue.length;
+    this.currentState.time = queue.start + queue.length;
     var buildableItem = <build.BuildableItem>queue.items[queue.items.length-1];
     buildableItem.initialTask = task;
     var reassignementItem = new assignments.ReassignmentItem(
