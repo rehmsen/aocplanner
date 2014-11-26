6cf2f7e4e76631729f38d108c645c645c2db0488
Ole Rehmsen <ole@rehmsen.de>
2014-11-26 21:35:43 +0100

    Generate version bumping tasks using a loop.

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -6,30 +6,14 @@ var shell = require('gulp-shell');
 var bump = require('gulp-bump');
 var git = require('gulp-git');
 
-gulp.task('bump:patch', function(){
-  gulp.src('./package.json')
-  .pipe(bump({type:'patch'}))
-  .pipe(gulp.dest('./'))
-  .pipe(git.commit('Bumping version.'));
-});
-
-gulp.task('bump:minor', function(){
-  gulp.src('./package.json')
-  .pipe(bump({type:'minor'}))
-  .pipe(gulp.dest('./'));
-});
-
-gulp.task('bump:major', function(){
-  gulp.src('./package.json')
-  .pipe(bump({type:'major'}))
-  .pipe(gulp.dest('./'));
-});
-
-// gulp.task('commit', ['bump:patch'], function(){
-//   return gulp.src('./package.json')
-//     .pipe(git.commit('initial commit'));
-// });
-
+['patch', 'minor', 'major'].forEach(function(type) {
+  gulp.task('bump:' + type, function() {
+    gulp.src('./package.json')
+      .pipe(bump({type: type}))
+      .pipe(gulp.dest('./'))
+      .pipe(git.commit('Bumping version.'));
+  });
+}); 
 
 gulp.task('deploy', ['dist'], shell.task([
   'goapp deploy dist'
