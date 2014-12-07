19a44c52586c441d8badc339152629a4694a8565
Ole Rehmsen <ole@rehmsen.de>
2014-12-07 14:43:05 +0100

    Can select multiple workers doing the same task - fixes #7.

--- src/client/components/aoe2/ui/task-count-directive.ts
+++ src/client/components/aoe2/ui/task-count-directive.ts
@@ -1,5 +1,7 @@
 /// <reference path="../../../../../typings/angularjs/angular.d.ts" />
 
+import core = require('../model/core');
+
 function createTaskCountDirective(): ng.IDirective {
   return {
     templateUrl: '/components/aoe2/ui/task-count.html',
@@ -7,8 +9,23 @@ function createTaskCountDirective(): ng.IDirective {
     scope: {
       taskCount: '&',
       selection: '='
-    }
+    },
+    controller: TaskCountDirectiveController,
+    controllerAs: 'ctrl',
+    bindToController: true
   };
+}
+
+class TaskCountDirectiveController {
+  taskCount: () => core.ITaskCount;
+  selection: core.Selection;
+
+  addOrSet() {
+    var added = this.selection.add(this.taskCount().assignable, this.taskCount().task);
+    if (!added) {
+      this.selection.set(this.taskCount().assignable, this.taskCount().task);
+    }
+  }
 }
 
 export = createTaskCountDirective;
